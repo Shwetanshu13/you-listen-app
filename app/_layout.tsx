eee3946c910f7b4882529c4d69e1b1c4fb6565c3
Shwetanshu13 <shwetanshusinha13@gmail.com>
2025-07-19 15:17:39 +0530

    Add Tailwind CSS configuration file for styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import { SafeAreaView } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import "./global.css";
 
 const RootLayout = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
       <Stack>
-        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen
+          name="index"
+          options={{ title: "Home", headerShown: false }}
+        />
       </Stack>
     </SafeAreaView>
   );
